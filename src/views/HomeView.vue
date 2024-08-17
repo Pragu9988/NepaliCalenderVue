7e1f862a47ae7c7195757e4eabbf50067ac42126
pragyan.chaudhary <chaudharypragyan@gmail.com>
2024-08-17 17:11:17 +0545

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Calender from '@/components/Calender.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container mx-auto py-10 mt-20">
+      <Calender />
+    </div>
+  </main>
+</template>
